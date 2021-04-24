f05c837e647b59feef84e3f8f0995473f6059aaa
Zaino <hello@darrin.dev>
2021-04-24 10:28:44 -0700

    successfully connected app to DB

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+// dependencies 
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
   host: 'localhost',
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: 'root',
-
   // Your password
-  password: 'password',
+  password: '', // MySQL not accepting a password value right now, fix with empty string
   database: 'fun_corpDB',
 });
+
+//* This is our basic connection to MySQL and output 
+function sqlTest() {
+    connection.query(`SELECT * FROM DEPARTMENT;`, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        //promptUser();
+    });
+  }
+
+  sqlTest();
+  
+  
---
+++ queries.sql
@@ -0,0 +1,8 @@
+-- Get all departments
+SELECT * FROM DEPARTMENT;
+
+-- Get all roles
+SELECT * FROM ROLE_;
+
+-- Get all employees
+SELECT * FROM EMPLOYEE;
